cc1912350e8433961d661308c5784698dbc5c270
baybeboy12 <quangdungzkh@gmail.com>
2023-09-11 11:05:04 +0700

    Create project, structure

--- src/main/java/vn/edu/iuh/fit/entities/Accoumt.java
+++ src/main/java/vn/edu/iuh/fit/entities/Accoumt.java
@@ -1,4 +1,10 @@
 package vn.edu.iuh.fit.entities;
 
 public class Accoumt {
+    private String account_id;
+    private String full_name;
+    private  String password;
+    private  String email;
+    private  String phone;
+    private  Integer status;
 }
